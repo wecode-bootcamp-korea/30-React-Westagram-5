eebcd1ad30664818f155c399684cf04fda619a40
YOOKIKI <ny90208@naver.com>
2022-02-19 20:58:29 +0900

    [30기 노유정]Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현  # Mission 1) Login | 사용자 입력 데이터 저장   - ID <input> 에서 onChange event 발생   - event 발생 시 handleIdInput 함수 실행   - handleIdInput 는 이벤트를 인자로 받음   - event가 일어난 요소에 담긴 value 값 (event.target.value)을 state에 저장   - 위의 과정을 PW <input> 에도 동일하게 적용

--- src/pages/yoojungNoh/Login/Login.js
+++ src/pages/yoojungNoh/Login/Login.js
@@ -1,11 +1,45 @@
-import './Login.scss';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.scss"
 
-function LoginYoojung() {
+// export default function Login() {
+//   return <div>Login</div>;
+// }
+
+// 혹은 const 함수 방법
+
+
+function Login() {
+  const input = useState();
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate("/Main");
+  };
+
+  const hadleIdInput = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <>
-      <div>hello</div>
+      <section>
+        <article>
+          <div className="loginBox">
+            <h1 className="logo">Westagram</h1>
+            <div>
+              <form className="loginForm">
+                <input type="text" id="idInput" placeholder="전화번호, 사용자 이름 또는 이메일" onChange={hadleIdInput} />
+                <input type="password" id="pwInput" placeholder="비밀번호" onChange={hadleIdInput} />
+              </form>
+              <button id="loginClick" className="loginButton" onClick={goToMain}>로그인</button>
+              <a className="forgotPwd">비밀번호를 잊으셨나요?</a>
+            </div>
+          </div>
+        </article>
+      </section>
     </>
-  );
-}
+  )
+};
 
-export default LoginYoojung;
+export default Login;
--- src/pages/yoojungNoh/Main/Nav/Nav.js
+++ src/pages/yoojungNoh/Main/Nav/Nav.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Nav.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.scss";
+import { BsSearch } from 'react-icons/bs';
+// import { VscZoomIn } from "react-icons/vsc";
 
 function Nav() {
+
   return (
     <>
       <nav>
@@ -11,7 +14,9 @@ function Nav() {
           <h1 className="logo">westagram</h1>
           <div className="inputBox">
             {/* <!-- 검색바 --> */}
-            <input type="password " id="password" placeholder="검색" />
+            <div className="searchInput"><BsSearch />
+              <input type="password " id="password" placeholder="검색" />
+            </div>
           </div>
           <div>
             <div>
@@ -19,16 +24,15 @@ function Nav() {
               <img className="navImg" src="/images/direct-instagram.PNG" />
               <img className="navImg" src="/images/heart.png" />
               {/* Link -> 메인 로그인화면으로 가는 "/"  &   user이미지 */}
-              <Link to="/">
-                <img className="navUserImg" src="/images/light.PNG" />
+              <Link to="/"><img className="navUserImg" src="/images/light.PNG" />
               </Link>
             </div>
           </div>
         </div>
       </nav>
-      <div className="navOutline" />
+      <div className="navOutline"></div>
     </>
-  );
+  )
 }
 
 export default Nav;
