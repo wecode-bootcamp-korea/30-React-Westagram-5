2648b4e3e33bf7cf9f9d414811f6296ccee2ef81
YOOKIKI <ny90208@naver.com>
2022-02-22 15:09:40 +0900

    Modify : branch 전체 수정

--- src/pages/yoojungNoh/Login/Login.js
+++ src/pages/yoojungNoh/Login/Login.js
@@ -1,11 +1,57 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.scss';
+import Button from './Button/Button';
 
-function LoginYoojung() {
+function Login() {
+  const [idInput, setIdInput] = useState('');
+  const [pwInput, setPwInput] = useState('');
+
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/Main');
+  };
+
+  const handleIdInput = e => {
+    setIdInput(e.target.value);
+  };
+
+  const handlePwInput = e => {
+    setPwInput(e.target.value);
+  };
+  const loginOn = idInput.includes('@') && pwInput.length >= 5;
   return (
-    <>
-      <div>hello</div>
-    </>
+    <section>
+      <article>
+        <div className="loginBox">
+          <h1 className="logo">Westagram</h1>
+          <div>
+            <form className="loginForm">
+              <input
+                type="text"
+                id="idInput"
+                placeholder="전화번호, 사용자 이름 또는 이메일"
+                onChange={handleIdInput}
+              />
+              <input
+                type="password"
+                id="pwInput"
+                placeholder="password"
+                value={pwInput}
+                onChange={handlePwInput}
+              />
+            </form>
+
+            <Button
+              onclick={goToMain}
+              className={loginOn ? 'activeBtn' : 'unActiveBtn'}
+            />
+            <a className="forgotPwd">비밀번호를 잊으셨나요?</a>
+          </div>
+        </div>
+      </article>
+    </section>
   );
 }
 
-export default LoginYoojung;
+export default Login;
