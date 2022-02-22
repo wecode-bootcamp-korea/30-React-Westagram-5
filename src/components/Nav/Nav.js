25204f0f2cf0d7ab0f8ce8942db9b47c3828d983
YOOKIKI <ny90208@naver.com>
2022-02-22 15:44:11 +0900

    Add : nav 추가

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.scss';
+import { BsSearch } from 'react-icons/bs';
+
 function Nav() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <div className="nav">
+          {/* <!-- 위스타그램 로고 --> */}
+          <h1 className="logo">westagram</h1>
+          <div className="inputBox">
+            {/* <!-- 검색바 --> */}
+            <div className="searchInput">
+              <BsSearch />
+              <input type="password " id="password" placeholder="검색" />
+            </div>
+          </div>
+          <div>
+            <div>
+              {/* <!-- 최상단 네비 아이콘 & 프로필이미지 --> */}
+              <img className="navImg" src="/images/direct-instagram.PNG" />
+              <img className="navImg" src="/images/heart.png" />
+              {/* Link -> 메인 로그인화면으로 가는 "/"  &   user이미지 */}
+              <Link to="/">
+                <img className="navUserImg" src="/images/light.PNG" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="navOutline" />
+    </>
+  );
 }
 
 export default Nav;
